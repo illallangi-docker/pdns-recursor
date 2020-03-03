e32306db1bad324b0f8ac83be667f9b8ca2ed752
Andrew Cole <andrew.cole@illallangi.com>
2020-03-03 18:46:58 +1100

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+UMASK=$(which umask)
+UMASK_SET=${UMASK_SET:-022}
+
+if [[ ! -x $UMASK ]]; then
+  echo "umask binary not found"
+  exit 1
+fi
+
+echo $UMASK "$UMASK_SET"
+$UMASK "$UMASK_SET"
+
+CONFD=$(which confd)
+
+if [[ ! -x $CONFD ]]; then
+  echo "confd binary not found"
+  exit 1
+fi
+
+echo $CONFD -onetime -backend env -log-level debug
+$CONFD -onetime -backend env -log-level debug || exit 1
+
+PDNS_RECURSOR=$(which pdns_recursor)
+if [[ ! -x $PDNS_RECURSOR ]]; then
+  echo "pdns_recursor binary not found"
+  exit 1
+fi
+
+cat ${PDNS_RECURSOR_CONFIG}
+echo ${*:-${PDNS_RECURSOR} ${PDNS_RECURSOR_CONFIG}}
+exec ${*:-${PDNS_RECURSOR} ${PDNS_RECURSOR_CONFIG}}
